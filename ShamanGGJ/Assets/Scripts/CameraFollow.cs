02cc31a8d322e5e43f35452111fc8cd5a80a58be
Marko Alerić <maleric@foi.hr>
2016-01-30 11:57:17 +0100

    Izmjena za player prefab, zapoceta kamera, dodane neka datoteke (iako je samo meta)

---
+++ ShamanGGJ/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraFollow:MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
